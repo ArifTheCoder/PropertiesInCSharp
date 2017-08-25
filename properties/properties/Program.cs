3ac57531fe89ede6fc82d1a7abcf40f7fb267cfa
ArifTheCoder <arifthecoder@gmail.com>
2017-08-25 21:15:38 +0300

    properties
    use of properties instead of function

---
+++ properties/properties/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class Program
+    {
+
+        public class Employee
+        {
+            private int _id;             //unused variable
+            private string _name;
+            private int _salary;
+
+            public int ID
+            {
+                set;
+                get;
+            }
+
+            public string Name
+            {
+                set
+                {
+                    this._name = value;
+                }
+                get
+                {
+                    return string.IsNullOrEmpty(this._name) ? "No name" : this._name;
+                }
+            }
+
+            public int Salary
+            {
+                set
+                {
+                    this._salary = value;
+                }
+                get
+                {
+                    return this._salary;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+
+            e1.ID = 10010;
+            e1.Name = "Mark";
+            e1.Salary = 5000;
+
+            Console.WriteLine("ID: {0}", e1.ID);
+            Console.WriteLine("Name: {0}", e1.Name);
+            Console.WriteLine("Salary: {0}", e1.Salary);
+
+            Console.ReadLine();
+
+        }
+    }
+}
